5bbf1598e2465b546dc8622f225b44ce36f70ff8
TylerOlli <ollityler@gmail.com>
2017-12-19 18:41:06 -0500

    Wrote function to reposition concert info

--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
 function change(){
 
-	if($(window).width() <= 700){
-  		$('#div2').html($('#div1').html());
+	if($(window).width() <= 768){
+  		$('.div2').html($('.div1').html());
 		}
-	else if (width > 770) {
-   		$('#div1').html($('#div2').html());
+	else if (width > 769) {
+   		$('.div1').html($('.div2').html());
  		}
 
 }
 
 setInterval(change, 10);
-
-//$(window).resize(function() { //Fires when window is resized
-//    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-//    if(width <= 770) {
- //       $('#div2').html($('#div1').html());
-		
- //   }
-
- //  else if (width > 770) {
-  //  	$('#div1').html($('#div2').html());
- //   }
-//})
