62573286e0c1f2fa5ee1a9fe9b25479915444513
TylerOlli <ollityler@gmail.com>
2017-12-19 19:22:23 -0500

    Finished making events fully mobile responsive

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@ function change(){
 	if($(window).width() <= 768){
   		$('.small-div-1').html($('.large-div-1').html());
   		$('.small-div-2').html($('.large-div-2').html());
+  		$('.small-div-3').html($('.large-div-3').html());
+  		$('.small-div-4').html($('.large-div-4').html());
+  		$('.small-div-5').html($('.large-div-5').html());
+  		$('.small-div-6').html($('.large-div-6').html());
 		}
 	else if (width > 769) {
    		$('.large-div-1').html($('.small-div-1').html());
    		$('.large-div-2').html($('.small-div-2').html());
+   		$('.large-div-3').html($('.small-div-3').html());
+   		$('.large-div-4').html($('.small-div-4').html());
+   		$('.large-div-5').html($('.small-div-5').html());
+   		$('.large-div-6').html($('.small-div-6').html());
  		}
 
 }
