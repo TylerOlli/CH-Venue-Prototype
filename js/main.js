4b6db74acc1fdaa097b702019c444c198807cd7d
TylerOlli <ollityler@gmail.com>
2017-12-20 00:25:44 -0500

    Created JavaScript function to display all videos

--- js/main.js
+++ js/main.js
@@ -33,7 +33,13 @@ function change1(){
 
 setInterval(change, 10);
 
-var url = "https://www.youtube.com/embed/pZGNblFCvp0"
+
+var url1 = "https://www.youtube.com/embed/zXtsGAkyeIo"
+var url2 = "https://www.youtube.com/embed/taViGR5VT1U"
+var url3 = "https://www.youtube.com/embed/QcIy9NiNbmo"
+var url4 = "https://www.youtube.com/embed/HaMq2nn5ac0"
+var url5 = "https://www.youtube.com/embed/O-zpOMYRi0w"
+var url6 = "https://www.youtube.com/embed/vX5fAb76r-U"
 
 
 $(function(){
@@ -45,8 +51,26 @@ $(function(){
 
 $(function(){
     $('.hamburger-button').click(function() {
-        $(".event-div").removeClass("col-md-4 col-sm-6");
-        $('.youtube').prepend('<iframe id="myIframe" width="560" height="315" style="float: right; top:0;" src="https://www.youtube.com/embed/pZGNblFCvp0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
- 		$('#myIframe').attr('src', url)
+    $(".event-div").removeClass("col-md-4 col-sm-6");
+
+    $('.youtube1').prepend('<iframe class="myIframe1" width="560" height="315" style="float: right; top:0;" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+	$('.myIframe1').attr('src', url1)
+
+	$('.youtube2').prepend('<iframe class="myIframe2" width="560" height="315" style="float: right; top:0;" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+	$('.myIframe2').attr('src', url2)
+
+	$('.youtube3').prepend('<iframe class="myIframe3" width="560" height="315" style="float: right; top:0;" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+	$('.myIframe3').attr('src', url3)
+
+	$('.youtube4').prepend('<iframe class="myIframe4" width="560" height="315" style="float: right; top:0;" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+	$('.myIframe4').attr('src', url4)
+
+	$('.youtube5').prepend('<iframe class="myIframe5" width="560" height="315" style="float: right; top:0;" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+	$('.myIframe5').attr('src', url5)
+
+	$('.youtube6').prepend('<iframe class="myIframe6" width="560" height="315" style="float: right; top:0;" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+	$('.myIframe6').attr('src', url6)
+
+
     });
 });
