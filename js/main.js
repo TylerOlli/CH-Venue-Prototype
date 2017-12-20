3aef8b08113cd9dbd9c543f211b49d7b855bb3c6
TylerOlli <ollityler@gmail.com>
2017-12-19 21:56:18 -0500

    Added new functionality to top buttons

--- js/main.js
+++ js/main.js
@@ -14,9 +14,21 @@ function change(){
    		$('.large-div-3').html($('.small-div-3').html());
    		$('.large-div-4').html($('.small-div-4').html());
    		$('.large-div-5').html($('.small-div-5').html());
-   		$('.large-div-6').html($('.small-div-6').html());
+   		$('.large-div-6').html($('.small-div-').html());
  		}
 
 }
 
 setInterval(change, 10);
+
+$(function(){
+    $('.dots-button').click(function() {
+        $(".event-div").addClass("col-md-4 col-sm-6");
+    });
+});
+
+$(function(){
+    $('.hamburger-button').click(function() {
+        $(".event-div").removeClass("col-md-4 col-sm-6");
+    });
+});
